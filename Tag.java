ef82fac83c88f17c056f557b7d231ab1129327f4
Timur <timur.turdaliev08@gmail.com>
2025-12-01 15:02:26 -0500

    Added classes

---
+++ Tag.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Tag - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Tag
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ Unit.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Unit - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Unit
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
